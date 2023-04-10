f3927b20881d523790553407cf2e6cd9950081f2
kevincallaghan <kevincallaghan5@yahoo.com>
2023-04-09 19:32:00 -0500

    first attempt at HTML divs, some CSS, README, and no JS yet

---
+++ script.js
@@ -0,0 +1,19 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+//TODO start button with onclick to start quiz
+//todo timer that counts down once started
+//todo question appears with answer choices
+//todo once answered, another question appears
+//todo timer subtracted if answer is wrong
+//todo quiz ends when all questions are answered of timer gets to zero
+//todo then game over shows up
+//todo then you can enter and save score
